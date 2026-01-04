6d6d42833641cfa5a9737dea4780985e884adef8
Christoffer Zakrisson <rustypig91@gmail.com>
2026-01-04 11:28:06 +0100

    Fix review comments

--- app/src/chessboard.h
+++ app/src/chessboard.h
@@ -10,7 +10,7 @@
 #define CHESS_FILE_F    ((uint8_t)5u)
 #define CHESS_FILE_G    ((uint8_t)6u)
 #define CHESS_FILE_H    ((uint8_t)7u)
-#define CHESS_NUM_RANKS ((uint8_t)8u)
+#define CHESS_NUM_FILES ((uint8_t)8u)
 
 #define CHESS_RANK_1    ((uint8_t)0u)
 #define CHESS_RANK_2    ((uint8_t)1u)
@@ -20,7 +20,7 @@
 #define CHESS_RANK_6    ((uint8_t)5u)
 #define CHESS_RANK_7    ((uint8_t)6u)
 #define CHESS_RANK_8    ((uint8_t)7u)
-#define CHESS_NUM_FILES ((uint8_t)8u)
+#define CHESS_NUM_RANKS ((uint8_t)8u)
 
 int chessboard_scan_file(uint8_t file);
 int chessboard_scan(void);
