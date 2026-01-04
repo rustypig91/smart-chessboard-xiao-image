4c3ef1a71d6008e5f8c13e4da5245527094b38d4
Christoffer Zakrisson <84663316+rustypig91@users.noreply.github.com>
2026-01-04 11:28:28 +0100

    Monitor threshold command (#1)

--- app/src/chessboard_cmd.c
+++ app/src/chessboard_cmd.c
@@ -3,6 +3,8 @@
 #include <zephyr/kernel.h>
 #include <zephyr/console/console.h>
 #include <zephyr/shell/shell.h>
+#include <stdlib.h>
+#include <string.h>
 
 char *shell_getline(const struct shell *shell, char *buf, const size_t len)
 {
@@ -39,7 +41,7 @@ static int print_mv(const struct shell *sh, int32_t (*get_value_func)(uint8_t, u
 
 	for (int rank = 7; rank >= 0; rank--) {
 		shell_fprintf(sh, SHELL_NORMAL, "%d", rank + 1);
-		for (int file = 0; file < 8; file++) {
+		for (int file = 0; file < CHESS_NUM_FILES; file++) {
 			shell_fprintf(sh, SHELL_NORMAL, "|");
 			val_mv = get_value_func(file, rank);
 			shell_fprintf(sh, SHELL_NORMAL, "%4d", val_mv);
@@ -57,7 +59,7 @@ static int print_mv_file(const struct shell *sh, int32_t (*get_value_func)(uint8
 	chessboard_scan_file(file);
 
 	shell_fprintf(sh, SHELL_NORMAL, "%c", 'A' + file);
-	for (int rank = 0; rank < 8; rank++) {
+	for (int rank = 0; rank < CHESS_NUM_RANKS; rank++) {
 		shell_fprintf(sh, SHELL_NORMAL, "|");
 		val_mv = get_value_func(file, rank);
 		shell_fprintf(sh, SHELL_NORMAL, "%4d", val_mv);
@@ -66,28 +68,103 @@ static int print_mv_file(const struct shell *sh, int32_t (*get_value_func)(uint8
 	return 0;
 }
 
-static int monitor_file(const struct shell *sh, int32_t (*get_value_func)(uint8_t, uint8_t))
+static bool monitor_command_shall_quit(const struct shell *sh)
 {
 	char c;
 	int ret;
 	size_t cnt;
+
+	ret = sh->iface->api->read(sh->iface, &c, sizeof(c), &cnt);
+	if (ret != 0) {
+		shell_error(sh, "Failed to read from shell (%d)", ret);
+		return true;
+	}
+
+	if (cnt == 1 && (c == 'q' || c == 'Q')) {
+		return true;
+	}
+
+	return false;
+}
+
+static int monitor_file(const struct shell *sh, int32_t (*get_value_func)(uint8_t, uint8_t))
+{
 	while (1) {
-		for (uint8_t file = CHESS_FILE_A; file <= CHESS_FILE_H; file++) {
+		for (uint8_t file = 0; file < CHESS_NUM_FILES; file++) {
 			print_mv_file(sh, get_value_func, file);
 		}
 
-		ret = sh->iface->api->read(sh->iface, &c, sizeof(c), &cnt);
-		if (ret != 0) {
-			shell_error(sh, "Failed to read from shell (%d)", ret);
-			return ret;
-		}
-
-		if (cnt == 1 && (c == 'q' || c == 'Q')) {
+		if (monitor_command_shall_quit(sh)) {
 			break;
 		}
 
 		k_yield();
 	}
+	return 0;
+}
+
+static int monitor_offset_threshold(const struct shell *sh, int32_t negative_threshold_mv,
+				    int32_t positive_threshold_mv, uint16_t hysteresis_mv)
+{
+#define STATE_NEGATIVE 0
+#define STATE_POSITIVE 1
+#define STATE_NEUTRAL  2
+#define STATE_UNKNOWN  3
+
+	uint8_t prev_state[CHESS_NUM_FILES][CHESS_NUM_RANKS];
+	for (int file = 0; file < CHESS_NUM_FILES; file++) {
+		for (int rank = 0; rank < CHESS_NUM_RANKS; rank++) {
+			prev_state[file][rank] = STATE_UNKNOWN;
+		}
+	}
+
+	uint8_t file = 0;
+
+	while (1) {
+		chessboard_scan_file(file);
+
+		for (uint8_t rank = 0; rank < CHESS_NUM_RANKS; rank++) {
+			const int32_t mv = chessboard_get_mv_offset(file, rank);
+
+			int32_t negative = negative_threshold_mv;
+			int32_t positive = positive_threshold_mv;
+
+			if (prev_state[file][rank] == STATE_POSITIVE) {
+				negative -= hysteresis_mv;
+				positive -= hysteresis_mv;
+			} else if (prev_state[file][rank] == STATE_NEGATIVE) {
+				negative += hysteresis_mv;
+				positive += hysteresis_mv;
+			} else if (prev_state[file][rank] == STATE_NEUTRAL) {
+				negative -= hysteresis_mv;
+				positive += hysteresis_mv;
+			}
+
+			const bool above = mv > positive;
+			const bool below = mv < negative;
+			const bool within = !above && !below;
+			if ((prev_state[file][rank] != STATE_POSITIVE) && above) {
+				shell_print(sh, "+%c%d", 'A' + file, rank + 1);
+				prev_state[file][rank] = STATE_POSITIVE;
+			} else if ((prev_state[file][rank] != STATE_NEGATIVE) && below) {
+				shell_print(sh, "-%c%d", 'A' + file, rank + 1);
+				prev_state[file][rank] = STATE_NEGATIVE;
+			} else if ((prev_state[file][rank] != STATE_NEUTRAL) && within) {
+				shell_print(sh, " %c%d", 'A' + file, rank + 1);
+				prev_state[file][rank] = STATE_NEUTRAL;
+			} else {
+				// no state change
+			}
+		}
+
+		if (monitor_command_shall_quit(sh)) {
+			break;
+		}
+
+		file = (file + 1) % CHESS_NUM_FILES;
+		k_yield();
+	}
+
 	return 0;
 }
 
@@ -101,6 +178,55 @@ static int cmd_board_monitor_file_offset_voltage(const struct shell *sh, size_t 
 {
 	monitor_file(sh, chessboard_get_mv_offset);
 	return 0;
+}
+
+static int cmd_board_monitor_offset_threshold(const struct shell *sh, size_t argc, char **argv)
+{
+	if ((argc < 3) || (argc > 4)) {
+		shell_print(sh, "Invalid number of arguments (%d)", argc);
+		shell_print(
+			sh,
+			"Usage: board monitor threshold <negative_threshold_mV> "
+			"<positive_threshold_mV> [hysteresis_mV]\n"
+			"- <negative_threshold_mV>: threshold for negative offset notification\n"
+			"- <positive_threshold_mV>: threshold for positive offset notification\n"
+			"- [hysteresis_mV]: optional hysteresis value to avoid notification "
+			"flooding\n");
+
+		return -EINVAL;
+	}
+
+	int err = 0;
+	long negative_threshold_mv = shell_strtol(argv[1], 10, &err);
+
+	if (err != 0) {
+		shell_error(sh, "Invalid negative threshold: %s", argv[1]);
+		return err;
+	}
+
+	long positive_threshold_mv = shell_strtol(argv[2], 10, &err);
+	if (err != 0) {
+		shell_error(sh, "Invalid positive threshold: %s", argv[2]);
+		return err;
+	} else if (positive_threshold_mv <= negative_threshold_mv) {
+		shell_error(sh, "Positive threshold must be greater than negative threshold");
+		return -ERANGE;
+	}
+
+	unsigned long hysteresis_mv = 0;
+	if (argc >= 4) {
+		hysteresis_mv = shell_strtoul(argv[3], 10, &err);
+		if (err != 0) {
+			shell_error(sh, "Invalid hysteresis: %s", argv[3]);
+			return err;
+		} else if (hysteresis_mv > UINT16_MAX) {
+			shell_error(sh, "Hysteresis too large: %s", argv[3]);
+			return -ERANGE;
+		}
+	}
+
+	return monitor_offset_threshold(sh, (int32_t)negative_threshold_mv,
+					(int32_t)positive_threshold_mv, (uint16_t)hysteresis_mv);
 }
 
 static int cmd_print_board_voltage(const struct shell *sh, size_t argc, char **argv)
@@ -140,12 +266,15 @@ SHELL_STATIC_SUBCMD_SET_CREATE(calib_cmds,
 					 cmd_set_board_calibration),
 			       SHELL_SUBCMD_SET_END);
 
-SHELL_STATIC_SUBCMD_SET_CREATE(monitor,
-			       SHELL_CMD(voltage, NULL, "Monitor chess board voltages",
-					 cmd_board_monitor_file_voltage),
-			       SHELL_CMD(offset, NULL, "Monitor chess board offset voltages",
-					 cmd_board_monitor_file_offset_voltage),
-			       SHELL_SUBCMD_SET_END);
+SHELL_STATIC_SUBCMD_SET_CREATE(
+	monitor,
+	SHELL_CMD(voltage, NULL, "Monitor chess board voltages", cmd_board_monitor_file_voltage),
+	SHELL_CMD(offset, NULL, "Monitor chess board offset voltages",
+		  cmd_board_monitor_file_offset_voltage),
+	SHELL_CMD(threshold, NULL,
+		  "Notify with hysteresis: threshold <negative_mV> <positive_mV> [hysteresis_mV]",
+		  cmd_board_monitor_offset_threshold),
+	SHELL_SUBCMD_SET_END);
 
 SHELL_STATIC_SUBCMD_SET_CREATE(
 	chess_cmds,
